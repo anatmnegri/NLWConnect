49925fcf157579359ca16d13b531252c55d3de93
anatmnegri <avanade.amsilva@voeazul.com.br>
2025-02-19 19:28:16 -0300

    Finalização tela 2

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -6,7 +6,7 @@ export function Button(props: ButtonProps) {
   return (
     <button
       className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
-      {...props} //o children já passa aqui
+      {...props} //o children já passa aqui 
     />
   );
 }
