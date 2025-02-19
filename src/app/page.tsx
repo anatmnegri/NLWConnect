3ead43ab9808e4bc1c451977d323627e5cee9fd8
anatmnegri <avanade.amsilva@voeazul.com.br>
2025-02-19 14:33:47 -0300

    Buttons component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { ArrowRight, Mail, Radio, User } from "lucide-react";
+import { Button } from "./components/button";
+import { IconButton } from "./components/icon-button";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button type="submit">
+        Enviar
+        <ArrowRight className="size-6" />
+      </Button>
+      <IconButton>
+        <Mail />
+      </IconButton>
     </main>
   );
 }
